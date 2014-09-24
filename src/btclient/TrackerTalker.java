32823cf9ae6d8ad05a2144a11d2595702569e93d
AdrianRMazur <adrianmaz@live.com>
2014-09-23 23:32:52 -0400

    merh tracker stuff

---
+++ src/btclient/TrackerTalker.java
@@ -0,0 +1,20 @@
+package btclient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TrackerTalker {
+
+	public static void MakeConnection(String ip, int port){
+		
+		BufferedReader br=null; 
+		Socket toTracker= new Socket(); 
+		
+		
+		
+		
+	}
+	
+	
+}
