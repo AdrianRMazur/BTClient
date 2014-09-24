1cbbd7442721c941c32a03a45fa6ddccb5c7b1c1
Kosti Papajani <kostipapajani@gmail.com>
2014-09-23 23:04:59 -0400

    moved stuff around

--- src/btclient/BTClient.java
+++ src/btclient/BTClient.java
@@ -1,11 +1,26 @@
 package btclient;
 
+import java.io.File;
+
 public class BTClient {
 
 	public static void main(String[] args) {
 
-		//this is a comment
-		// more comment
+		
+		if (args.length!=2){
+			System.out.println("Error: Provide torrent file name and save file name. \n");
+			System.exit(1);
+		} 
+		
+		File torrent = new File (args[0]);
+		int torrentsize = (int) torrent.length();
+		
+		byte[] torrentdata = new byte[torrentsize];
+		
+		
+		
+		
+		
 	}
 
 }
