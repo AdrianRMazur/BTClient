32e6692a7c6739aea17ca241d1bcbd8ef5cf85ae
Kosti Papajani <kostipapajani@gmail.com>
2014-09-29 22:40:01 -0400

    idk

--- src/btclient/BTClient.java
+++ src/btclient/BTClient.java
@@ -2,12 +2,15 @@ package btclient;
 
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
@@ -57,8 +60,31 @@ public class BTClient {
 		String ip = url.getHost(); 
 		int port = url.getPort();
 	
-		
 	
+		
+		HttpURLConnection con =null;
+		BufferedReader in = null;
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		try {
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			int responseCode = con.getResponseCode();
+			in = new BufferedReader( new InputStreamReader(con.getInputStream()));
+			 
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	
+		System.out.println(response.toString());
+
 		
 		//good shit
 		System.out.println("path is: " + ip + "\n port is: " + port);
